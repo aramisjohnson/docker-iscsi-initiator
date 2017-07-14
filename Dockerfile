b98cbadb39656fe9811f8dc8b7044fd84fbec293
aramisjohnson <ty.johnson@quantum.com>
2017-07-14 15:38:46 -0700

    bullet proofed script, added readwrite actions

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM debian
-MAINTAINER keepwn <keepwn@gmail.com>
+MAINTAINER aramisjohnson <aramis.johnson2010@gmail.com>
 
 RUN apt-get update
 RUN apt-get install open-iscsi
 
-RUN mkdir /mnt/storage
+RUN mkdir -p /mnt/storage
 WORKDIR /tmp
 COPY ./mount-iscsi.sh ./
 RUN chmod +x ./mount-iscsi.sh
@@ -13,5 +13,6 @@ ENV IP 192.168.0.1
 ENV PORT 3260
 ENV TARGETNAME iqn.2005-10.org.freenas.ctl:mystorage
 ENV LUN 0
+ENV DATA anything
 
 ENTRYPOINT ["/bin/bash","/tmp/mount-iscsi.sh"]
--- mount-iscsi.sh
+++ mount-iscsi.sh
@@ -1,31 +1,85 @@
 #!/bin/bash
 
+
+
 mount_iscsi() {
-    echo "Restarting iSCSI"
+    echo >&2 "Restarting iSCSI"
     service open-iscsi restart
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
 
-    echo "Discovering and mounting"
-    iscsiadm -m discovery -t st -p $IP
+    echo >&2 "Discovering targets"
+    iscsiadm -m discovery -t sendtargets -p $IP:$PORT
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
+
+    echo >&2 "Logging into scsi target"
     iscsiadm -m node --targetname "$TARGETNAME" --portal "$IP:$PORT" --login
-    sleep 1
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
+    sleep 5
 
-    echo "Mounting new iSCSI-device"
-    mount /dev/disk/by-path/ip-$IP:$PORT-iscsi-$TARGETNAME-lun-0-part1 /mnt/storage
+    echo >&2 "Formatting iscsi device"
+    local iscsi_device=`lsblk --scsi | grep -F -i 'iscsi' | awk '{print $1}'`
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
+
+    arr=($iscsi_device)
+    mkfs.ext4 -F /dev/${arr[0]}
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
+
+    mkdir -p /mnt/iscsi
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
+
+    echo >&2 "Mounting new iSCSI-device"
+    mount /dev/${arr[0]} /mnt/iscsi
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
 }
 
 umount_iscsi() {
-    echo "Umounting iSCSI-mount"
-    umount /mnt/storage
+    echo >&2 "Umounting iSCSI-mount"
+    umount /mnt/iscsi
+
     iscsiadm -m node --targetname "$TARGETNAME" --portal "$IP:$PORT" --logout
-    echo "Umount finished"
 }
 
-trap "echo 'will stop';umount_iscsi;exit" SIGHUP SIGINT SIGQUIT SIGTERM
+write_data() {
+    echo >&2 "Writing data to mounted device"
+    echo $DATA > /mnt/iscsi/file.txt
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
+}
+
+read_compare_data() {
+    echo >&2 "Reading file from mounted device"
+    out=$(cat /mnt/iscsi/file.txt)
+    rc=$?; if [[ $rc != 0 ]]; then return "$rc"; fi
+
+    if [ "$out" != "$DATA" ]; then
+        echo >&2 "Failed to read from file on mounted device"
+        return 1
+    fi
+}
+
+cleanup() {
+    echo >&2 "Failure Occurred"
+    umount_iscsi
+    echo >&2 "Exit Code is $rc"
+    exit $rc
+}
+
+trap cleanup SIGHUP SIGINT SIGQUIT SIGTERM
 
 # Mount
 mount_iscsi
-read -p "[enter key to exit]"
+rc=$?
+
+if [[ $rc != 0 ]]; then kill -HUP $$; fi
+
+
+# Read/Write
+if [ ! -z "$DATA" ]; then
+    write_data
+    read_compare_data
+fi
 
 # Umount
 umount_iscsi
-echo "exited $0"
+
+echo "exited $rc"
+
